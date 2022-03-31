e0611c360843aed4f1bba9a6b1c5c6d95436e3ab
superlativesecretsociety <superlativesecretsociety@gmail.com>
2022-04-01 02:51:12 +0700

    setup webpack: babel, css

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import "./styles.css";
+
+import { camelCase } from "lodash";
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+  return <div>App Component</div>;
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require("path");
+
+module.exports = {
+  mode: "production",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/i,
+        exclude: /node_modules/,
+        use: ["babel-loader"],
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
